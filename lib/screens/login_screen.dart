90c0b481740a954df7cc571e36d9ccf76365cd70
Ivan Sholana <ivansholana@gmail.com>
2023-09-05 16:19:24 +0700

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginScreenState();
+  }
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
