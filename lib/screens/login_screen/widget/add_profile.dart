fe0ed8908867c8410f655b6339f438d65d186ede
Ivan Sholana <ivansholana@gmail.com>
2023-09-06 07:47:59 +0700

    profile screen done

--- lib/screens/login_screen/widget/add_profile.dart
+++ lib/screens/login_screen/widget/add_profile.dart
@@ -1,5 +1,7 @@
-import 'package:chat_app/main.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:intl/intl.dart';
 
 class AddProfile extends StatefulWidget {
   const AddProfile({super.key});
@@ -12,6 +14,7 @@ class _AddProfileState extends State<AddProfile> {
   String _gender = "";
   TextEditingController _dateController = TextEditingController();
   DateTime selectedDate = DateTime.now();
+  File? pickedImage;
   Future<void> _selectDate() async {
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -22,7 +25,17 @@ class _AddProfileState extends State<AddProfile> {
     if (picked != null && picked != selectedDate) {
       setState(() {
         selectedDate = picked;
-        _dateController.text = selectedDate.toLocal().toString().split(' ')[0];
+        _dateController.text = DateFormat('yyyy-MM-dd').format(selectedDate);
+      });
+    }
+  }
+
+  void _takeImage() async {
+    final imagePick = await ImagePicker().pickImage(
+        source: ImageSource.camera, maxWidth: 400, imageQuality: 100);
+    if (imagePick != null) {
+      setState(() {
+        pickedImage = File(imagePick.path);
       });
     }
   }
@@ -47,141 +60,147 @@ class _AddProfileState extends State<AddProfile> {
           ),
         )),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(30),
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50),
-                  bottomRight: Radius.circular(50)),
-              gradient: LinearGradient(
-                  colors: [Colors.purple, Colors.blueAccent],
-                  begin: Alignment.topLeft,
-                  end: Alignment.topRight),
-            ),
-            child: InkWell(
-              child: Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.blueGrey.withOpacity(0.5)),
-                child: const Icon(
-                  Icons.camera_alt,
-                  color: Colors.white,
-                  size: 50,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              clipBehavior: Clip.hardEdge,
+              padding: const EdgeInsets.all(30),
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50)),
+                gradient: LinearGradient(
+                    colors: [Colors.purple, Colors.blueAccent],
+                    begin: Alignment.topLeft,
+                    end: Alignment.topRight),
+              ),
+              child: InkWell(
+                onTap: () => _takeImage(),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blueGrey.withOpacity(0.5)),
+                  child: pickedImage == null
+                      ? const Icon(
+                          Icons.camera_alt,
+                          color: Colors.white,
+                          size: 50,
+                        )
+                      : CircleAvatar(
+                          backgroundImage: FileImage(pickedImage!),
+                        ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            "Enter your data profile",
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall!
-                .copyWith(color: Colors.black),
-          ),
-          Text("Ensure your data profile is correct"),
-          Container(
-            padding: EdgeInsets.all(25),
-            child: Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    initialValue: "ivan@gmail.com",
-                    enabled: false,
-                    decoration: InputDecoration(
-                      filled: true,
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Enter your data profile",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(color: Colors.black),
+            ),
+            const Text("Ensure your data profile is correct"),
+            Container(
+              padding: const EdgeInsets.all(25),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      initialValue: "ivan@gmail.com",
+                      enabled: false,
+                      decoration: const InputDecoration(
+                        filled: true,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                              filled: true, labelText: "First Name"),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                filled: true, labelText: "First Name"),
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                              filled: true, labelText: "Last Name"),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                filled: true, labelText: "Last Name"),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      keyboardType: TextInputType.phone,
+                      decoration: const InputDecoration(
+                          filled: true, labelText: "Phone Number"),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          child: DropdownButtonFormField(
+                            value: "Male",
+                            items: ['Male', 'Female'].map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (newValue) {
+                              setState(() {
+                                _gender = newValue!;
+                              });
+                            },
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    keyboardType: TextInputType.phone,
-                    decoration: const InputDecoration(
-                        filled: true, labelText: "Phone Number"),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Expanded(
-                        child: DropdownButtonFormField(
-                          value: "Male",
-                          items: ['Male', 'Female'].map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (newValue) {
-                            setState(() {
-                              _gender = newValue!;
-                            });
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 20),
-                      Expanded(
-                        child: TextFormField(
-                          readOnly: true,
-                          controller: _dateController,
-                          decoration: InputDecoration(
-                              labelText: "Date",
-                              prefixIcon: InkWell(
-                                onTap: () => _selectDate(),
-                                child: Icon(Icons.date_range),
-                              )),
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: TextFormField(
+                            readOnly: true,
+                            controller: _dateController,
+                            decoration: InputDecoration(
+                                labelText: "Birth Date",
+                                prefixIcon: InkWell(
+                                  onTap: () => _selectDate(),
+                                  child: const Icon(Icons.date_range),
+                                )),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          InkWell(
-            child: Container(
-              margin: EdgeInsets.all(20),
-              height: 50,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  gradient: LinearGradient(
-                      colors: [Colors.purple, Colors.blueAccent])),
-              child: Text(
-                "Submit Data",
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+            InkWell(
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                height: 50,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    gradient: LinearGradient(
+                        colors: [Colors.purple, Colors.blueAccent])),
+                child: Text(
+                  "Submit Data",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                width: double.infinity,
               ),
-              width: double.infinity,
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
